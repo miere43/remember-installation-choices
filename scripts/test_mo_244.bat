14581725c81b2614bee1d0e219a90be50e61917c
miere43 <x.miere@gmail.com>
2025-02-03 17:46:59 +0300

    Added test scripts.

---
+++ scripts/test_mo_244.bat
@@ -0,0 +1,4 @@
+del /y C:\soft\mo2_244_oldver\plugins\remember_installation_choices\__init__.py
+del /y C:\soft\mo2_244_oldver\plugins\remember_installation_choices\__pycache__
+copy C:\dev\remember_installation_choices\__init__.py C:\soft\mo2_244_oldver\plugins\remember_installation_choices\__init__.py
+C:\soft\mo2_244_oldver\ModOrganizer.exe
---
+++ scripts/test_mo_252.bat
@@ -0,0 +1,4 @@
+del /y C:\soft\mo2\plugins\remember_installation_choices\__init__.py
+del /y C:\soft\mo2\plugins\remember_installation_choices\__pycache__
+copy C:\dev\remember_installation_choices\__init__.py C:\soft\mo2\plugins\remember_installation_choices\__init__.py
+C:\soft\mo2\ModOrganizer.exe
